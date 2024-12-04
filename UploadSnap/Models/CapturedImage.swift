fecf9d44b0747df22a926d43bd4a637f6d234314
rtsranjit <73750895+rtsranjit@users.noreply.github.com>
2024-12-04 23:24:35 +0530

    Changed

--- UploadSnap/Models/CapturedImage.swift
+++ UploadSnap/Models/CapturedImage.swift
@@ -8,17 +8,15 @@
 import Foundation
 import RealmSwift
 
-// CapturedImage Model
 class CapturedImage: Object, Identifiable {
-    @objc dynamic var id = UUID().uuidString // Ensure `id` is unique
+    @objc dynamic var id = UUID().uuidString
     @objc dynamic var name = ""
-    @objc dynamic var imageData = Data() // Ensure this is a valid data format
+    @objc dynamic var imageData = Data()
     @objc dynamic var url: String = ""
     @objc dynamic var uploadStatus: String = UploadStatus.pending.rawValue
     @objc dynamic var uploadProgress: Double = 0.0
 }
 
-// Enum for upload status
 enum UploadStatus: String {
     case pending = "Pending"
     case uploading = "Uploading"
